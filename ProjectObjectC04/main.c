04f1ddee7019f4587cdb23b915e81d982baeac10
lingdian <lingdian@meili-inc.com>
2017-08-18 17:59:33 +0800

    Initial Commit

---
+++ ProjectObjectC04/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ProjectObjectC04
+//
+//  Created by lingdian on 17/8/18.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
